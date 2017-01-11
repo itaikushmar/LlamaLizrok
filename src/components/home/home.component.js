4d122251ac0bf51419ee5fc39eea5c2d58b65d59
Itai <itaikushmar@gmail.com>
2017-01-11 14:54:01 +0200

    first commit

---
+++ src/components/home/home.component.js
@@ -0,0 +1,26 @@
+export default  {
+  data: () => {
+    return {
+      items: [],
+      contactForm: {name: '', email: '', phone: '', msg: ''},
+      formHidden: false
+    }
+  },
+  methods : {
+    sendForm () {
+      console.log('clicked' , this.contactForm);
+      this.$http.post('http://localhost:3003/data/contact', this.contactForm)
+            .then(() => {
+                this.toggleForm();
+                this.contactForm = {name: '', email: '', phone: '', msg: ''};
+            })
+    },
+    toggleForm () {
+      this.formHidden = true; 
+      setTimeout (() => {
+        this.formHidden = false;
+        console.log('formhidden' , this.formHidden);
+      }, 5000)
+    }
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import './style.scss';
+import './vendor';
+import './vue-plugins';
+
+import Vue from 'vue';
+import store from './store';
+import router from './routes';
+import VueResource from 'vue-resource'
+import MainNav from './components/main-nav';
+
+const app = new Vue({
+  router,
+  store,
+  components: {
+    MainNav
+  }
+}).$mount('#app');
+
