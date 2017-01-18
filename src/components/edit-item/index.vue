a9e5cd2df244ee3525c5246e54909f98b62e0000
Itai <itaikushmar@gmail.com>
2017-01-18 16:14:22 +0200

    pls

--- src/components/edit-item/index.vue
+++ src/components/edit-item/index.vue
@@ -1,5 +1,5 @@
 <template src="./edit-item.component.html"></template>
 <script src="./edit-item.component.js"></script>
-<style src="./edit-item.component.scss" scoped></style>
+<style src="./edit-item.component.scss" scoped lang="scss"></style>
 
 
